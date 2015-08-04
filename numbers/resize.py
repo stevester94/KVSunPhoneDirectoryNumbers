fe08171eaf95ab14b275186410eea78550f96ac3
Steven <stevester94@gmail.com>
2015-08-03 20:23:55 -0700

    resizing working on linux

---
+++ numbers/resize.py
@@ -0,0 +1,13 @@
+#resize all images in formatted
+
+from os import listdir
+from subprocess import call
+
+files = listdir("./formatted")
+len(files)
+counter = 0
+for f in files:
+	print counter
+	call(["convert", "./formatted/" + f, "-resize", "600x", "./resized/" + f])
+	counter = counter + 1
+		
