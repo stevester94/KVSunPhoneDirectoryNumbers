e95c43307c37accfa5050f3b0cd3108b3e8111ad
Steven Mackey <stevester94@gmail.com>
2015-10-15 00:50:01 -0700

    Changed Barnes representation in categories

--- numbers/regexTesting.py
+++
@@ -1,8 +0,0 @@
-#reg ex testing 
-
-import re
-
-regex = r"[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]"
-compiledReg = re.compile(regex)
-
-print compiledReg.search("Youngman, Richard   4608 Lake Isabella Blvd #11, Lk Isbl	379-6364")
