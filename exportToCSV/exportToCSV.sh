c5bbe781c87475c5d617ea1fa2f55917f1b2928b
Steven Mackey <stevester94@gmail.com>
2015-12-25 12:34:20 -0800

    working on replacing line breaks

--- exportToCSV/exportToCSV.sh
+++ exportToCSV/exportToCSV.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 sqlite3 -header -csv entries.db "select * from Entries;" > entries.csv
 sqlite3 -header -csv entries.db "select * from Categories;" > categories.csv
 sqlite3 -header -csv entries.db "select * from CategoriesList;" > categoryList.csv
